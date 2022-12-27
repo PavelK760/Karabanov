e7c1288fd2fa1fdd7cee605e130894f398d49a5d
PavelK760 <104727697+PavelK760@users.noreply.github.com>
2022-12-27 11:44:18 +0500

    Update and rename main.py to 4.2.2.py

---
+++ 4.2.2.py
@@ -0,0 +1,24 @@
+from report import InputConnect
+
+VAC = 'вакансии'
+STAT = 'статистика'
+INPUTS = (VAC, STAT)
+TEXT_IN = 'Выводим вакансии или статистику?'
+TEXT_ER = 'Некорректное значение ввода, попробуйте, ещё раз.'
+NAME_FILE = '../vacancies_by_year.csv'
+NAME_VAC = 'Программист'
+
+if __name__ == '__main__':
+    while True:
+        res = str(input(TEXT_IN)).lower()
+        if res.lower() in INPUTS:
+            break
+        print(TEXT_ER)
+
+    inputconnect = InputConnect(NAME_FILE, NAME_VAC)
+
+    if res == STAT:
+        inputconnect.gen_stats(True)
+    elif res == VAC:
+        statist1, statist2, statist3, statist4, statist5, statist6 = inputconnect.gen_stats()
+        inputconnect.gen_vac(statist1, statist2, statist3, statist4, statist5, statist6)
